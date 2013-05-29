e0559c34ee2db6f52a038ec429c741f1c7738791
yewton <yewton@gmail.com>
2013-05-30 00:13:05 +0900

    cf. Emacs Helmのパス自動補完がじゃまなので無効化した http://fukuyama.co/nonexpansion

--- common.el
+++ common.el
@@ -4,6 +4,11 @@
 ;;;; helm
 (require 'helm-config)
 (helm-mode 1)
+;; 自動補完を無効
+(custom-set-variables '(helm-ff-auto-update-initial-value nil))
+;; TABで任意補完。選択肢が出てきたらC-nやC-pで上下移動してから決定することも可能
+(defvar helm-read-file-map)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
 ;;;; auto-async
 (require 'auto-async-byte-compile)
