37e188a49a900162742d4d4e8c6192737478895a
yewton <yewton@gmail.com>
2013-05-30 00:26:25 +0900

    helm のべんり設定追加

--- common.el
+++ common.el
@@ -9,6 +9,8 @@
 ;; TABで任意補完。選択肢が出てきたらC-nやC-pで上下移動してから決定することも可能
 (defvar helm-read-file-map)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 
 ;;;; auto-async
 (require 'auto-async-byte-compile)
