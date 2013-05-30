f3da547c2cb5c0a01ae5f446b2013b7661a8c885
yewton <yewton@gmail.com>
2013-05-31 01:53:40 +0900

    scala-run-scala とかもあったほうが実際ベンリ

--- init.el
+++ init.el
@@ -66,7 +66,7 @@
   '(el-get migemo apel auto-async-byte-compile cl-lib
     magit helm helm-migemo open-junk-file buffer-move
     markdown-mode jaspace emacs-w3m dsvn yasnippet
-    maxframe ruby-mode ruby-block ruby-electric ruby-end
+    maxframe ruby-mode ruby-block ruby-electric ruby-end scala-mode
     auto-complete flymake flymake-ruby php-mode-improved scala-mode2
     helm-gtags bookmark+ php-eldoc php-completion ensime))
 (el-get 'sync cfg:packages)
