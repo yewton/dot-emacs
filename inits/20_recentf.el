255af534abcb26ce36ad2857185379b9ad7d3530
yewton <yuto_sasaki@dwango.co.jp>
2013-10-28 18:33:13 +0900

    add settings for recentf

--- inits/20_recentf.el
+++ inits/20_recentf.el
@@ -1,3 +1,6 @@
+(custom-set-variables
+ `(recentf-save-file ,(concat user-emacs-directory "recentf/history"))
+ '(recentf-max-saved-items 50))
 (require 'recentf-ext)
 (defadvice recentf-open-files (after recentf-set-overlay-directory-adv activate)
   (set-buffer "*Open Recent*")
