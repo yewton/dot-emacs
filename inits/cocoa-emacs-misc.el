738add48c59bab6dea24b895e722eec6c8452549
佐々木　悠人 <yuto_sasaki@ysasaki.local>
2013-12-16 01:03:13 +0900

    add emacs frame settings for cocoa

---
+++ inits/cocoa-emacs-misc.el
@@ -0,0 +1,10 @@
+;; Configuring Emacs on Mac OS X <http://korewanetadesu.com/software/emacs-on-os-x.html>
+(when (featurep 'ns)
+  (defun ns-raise-emacs ()
+    "Raise Emacs."
+    (ns-do-applescript "tell application \"Emacs\" to activate"))
+
+  (when (display-graphic-p)
+    (add-hook 'server-visit-hook 'ns-raise-emacs)
+    (add-hook 'before-make-frame-hook 'ns-raise-emacs)
+    (ns-raise-emacs)))
