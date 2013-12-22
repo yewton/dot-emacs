9430f02353f540ce7e43d20d794d6d8a7c4f14f5
yewton <yewton@gmail.com>
2013-12-22 12:18:47 +0900

    use `ns-toggle-fullscreen` if `window-system` is `'ns`

--- inits/70_maxframe.el
+++ inits/70_maxframe.el
@@ -1,13 +1,15 @@
 (when (display-graphic-p)
-  (eval-and-compile (require 'maxframe))
-  (defvar my:frame-max-flag nil)
-  (defun my:toggle-frame-size ()
-    (interactive)
-    (if my:frame-max-flag
-        (progn
-          (restore-frame)
-          (setq my:frame-max-flag nil))
-      (maximize-frame)
-      (setq my:frame-max-flag t)))
-  (add-hook 'window-setup-hook 'maximize-frame t)
+  (if (eq window-system 'ns)
+      (defun my:toggle-frame-size () (ns-toggle-fullscreen))
+    (eval-and-compile (require 'maxframe))
+    (defvar my:frame-max-flag nil)
+    (defun my:toggle-frame-size ()
+      (interactive)
+      (if my:frame-max-flag
+          (progn
+            (restore-frame)
+            (setq my:frame-max-flag nil))
+        (maximize-frame)
+        (setq my:frame-max-flag t))))
+  (add-hook 'window-setup-hook 'my:toggle-frame-size t)
   (global-set-key [f11] 'my:toggle-frame-size))
