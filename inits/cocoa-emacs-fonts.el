f5af35bc1366a158b2e5114c02f1acdbd3c811d1
yewton <yuto_sasaki@dwango.co.jp>
2014-01-07 22:58:40 +0900

    use Ricty Discord instead of plain Ricty

--- inits/cocoa-emacs-fonts.el
+++ inits/cocoa-emacs-fonts.el
@@ -3,7 +3,7 @@
  "Ricty-14:weight=normal:slant=normal" nil "ricty")
 (set-fontset-font "fontset-ricty"
                   'unicode
-                  (font-spec :family "Ricty" :size 14)
+                  (font-spec :family "Ricty Discord for Powerline" :size 14)
                   nil
                   'append)
 (add-to-list 'default-frame-alist '(font . "fontset-ricty"))
