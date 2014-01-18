fef668a01e913d64f359b2608e298a0fd99662f1
yewton <yewton@gmail.com>
2014-01-18 20:22:03 +0900

    fancy modeline

---
+++ inits/03_modeline.el
@@ -0,0 +1,125 @@
+(eval-when-compile (require 'cl))
+
+(set-face-attribute 'mode-line nil :box nil)
+(set-face-attribute 'mode-line-inactive nil :box nil)
+;; mode-lineのモード情報をコンパクトに表示する - Life is very short
+;; <http://d.hatena.ne.jp/syohex/20130131/1359646452>
+(defvar mode-line-cleaner-alist)
+(setq mode-line-cleaner-alist
+  '( ;; For minor-mode, first char is 'space'
+    (yas-minor-mode . " Ys")
+    (eldoc-mode . "")
+    (abbrev-mode . "")
+    (helm-gtags-mode . " HG")
+    (flymake-mode . " Fm")
+    (helm-mode . "")
+    (anzu-mode . " 杏")
+    ;; Major modes
+    (lisp-interaction-mode . "Li")
+    (python-mode . "Py")
+    (ruby-mode . "Rb")
+    (emacs-lisp-mode . "EL")
+    (markdown-mode . "Md")))
+(defun my:clean-mode-line ()
+  (interactive)
+  (loop for (mode . mode-str) in mode-line-cleaner-alist
+        do
+        (let ((old-mode-str (cdr (assq mode minor-mode-alist))))
+          (when old-mode-str
+            (setcar old-mode-str mode-str))
+          ;; major mode
+          (when (eq mode major-mode)
+            (setq mode-name mode-str)))))
+(add-hook 'after-change-major-mode-hook 'my:clean-mode-line)
+
+;; emacs:個性的なモードラインを紹介 | mgrace
+;; <http://mgrace.info/?p=886>
+
+;; Extra mode line faces
+(make-face 'mode-line-read-only-face)
+(make-face 'mode-line-modified-face)
+(make-face 'mode-line-folder-face)
+(make-face 'mode-line-filename-face)
+(make-face 'mode-line-mode-face)
+(make-face 'mode-line-minor-mode-face)
+(make-face 'mode-line-misc-info-face)
+
+(set-face-attribute 'mode-line-read-only-face nil
+    :inherit 'mode-line-face
+    :weight 'ultra-bold
+    :foreground "DarkGreen"
+    :background "LightGreen")
+(set-face-attribute 'mode-line-modified-face nil
+    :inherit 'mode-line-face
+    :weight 'ultra-bold
+    :foreground "DarkRed"
+    :background "goldenrod")
+(set-face-attribute 'mode-line-folder-face nil
+    :inherit 'mode-line-face
+    :foreground "gray60")
+(set-face-attribute 'mode-line-filename-face nil
+    :inherit 'mode-line-face
+    :foreground "yellow"
+    :weight 'ultra-bold)
+(set-face-attribute 'mode-line-mode-face nil
+    :inherit 'mode-line-face
+    :weight 'ultra-bold
+    :foreground "gray80"
+    :height 105)
+(set-face-attribute 'mode-line-minor-mode-face nil
+    :inherit 'mode-line-mode-face
+    :weight 'normal
+    :foreground "gray40"
+    :height 100)
+(set-face-attribute 'mode-line-misc-info-face nil
+    :inherit 'mode-line-face
+    :foreground "gray80")
+
+;; Helper function
+(defun my:shorten-directory (dir max-length)
+  "Show up to `max-length' characters of a directory name `dir'."
+  (let ((path (reverse (split-string (abbreviate-file-name dir) "/")))
+        (output ""))
+    (when (and path (equal "" (car path)))
+      (setq path (cdr path)))
+    (while (and path (< (length output) (- max-length 4)))
+      (setq output (concat (car path) "/" output))
+      (setq path (cdr path)))
+    (when path
+      (setq output (concat ".../" output)))
+    output))
+
+(setq-default mode-line-format
+ '("%e"
+   mode-line-mule-info
+   "%4lL"
+   " "
+   mode-line-client
+   mode-line-remote
+   " "
+   ;;; read-only or modified status
+   (:eval
+    (cond (buffer-read-only
+           (propertize " RO " 'face 'mode-line-read-only-face))
+          ((buffer-modified-p)
+           (propertize " ** " 'face 'mode-line-modified-face))
+          (t " ")))   mode-line-frame-identification
+   "   "
+   ;;; directory and buffer/file name
+   (:propertize (:eval (my:shorten-directory default-directory 30))
+                face mode-line-folder-face)
+   (:propertize "%b"
+                face mode-line-filename-face)
+   "(%p)"
+   ;;; print Narrow if appropriate.
+   " %n "
+   (vc-mode vc-mode)
+   "  %["
+   (:propertize mode-name
+                face mode-line-mode-face)
+   "%]"
+   (:eval (propertize (format-mode-line minor-mode-alist)
+                      'face 'mode-line-minor-mode-face))
+   "  "
+   (:propertize mode-line-misc-info face mode-line-misc-info-face)
+   mode-line-end-spaces))
